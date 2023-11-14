e12b44658125080745d11f92c591055800008d4f
Bang <thbang727@gmail.com>
2023-11-15 02:47:05 +0700

    plan for welcome

--- src/main/java/com/dictionary/Controllers/WelcomeController.java
+++ src/main/java/com/dictionary/Controllers/WelcomeController.java
@@ -1,11 +1,13 @@
 package com.dictionary.Controllers;
 
 import com.dictionary.Models.Model;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.media.MediaView;
 
 import java.net.URL;
@@ -18,10 +20,27 @@ public class WelcomeController implements Initializable {
     @FXML
     private MediaView mediaView;
 
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private Button registerBtn;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    private AnchorPane pane;
+
     private final MediaBackground mediaBackground = new MediaBackground("src/main/resources/Video/HomeBackground.mp4");
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        password.setFocusTraversable(false);
+        username.setFocusTraversable(false);
         try {
             mediaBackground.playVideo(mediaView,1);
         } catch (InterruptedException e) {
@@ -30,5 +49,18 @@ public class WelcomeController implements Initializable {
         enterButton.setOnAction(event -> Model.getInstance().getViewFactory().showWindow());
         mediaView.setFitHeight(510);
         mediaView.setFitWidth(900);
+    }
+
+    public void untarget() {
+        pane.requestFocus();
+    }
+
+    public void login() {
+    }
+
+    public void enterButton() {
+    }
+
+    public void register() {
     }
 }
